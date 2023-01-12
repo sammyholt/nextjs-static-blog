163e9521b64b7a21bcc1f0792f8809f46435ff95
Samuel Holt <samuel.l.holt@gmail.com>
2023-01-11 21:36:20 -0800

    Added function to get post data from files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,44 @@
 import Layout from "@/components/Layout";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Post from "@/types/Post";
 
-export default function HomePage() {
+type HomePageProps = {
+  posts: Post[];
+};
+
+export default function HomePage({ posts }: HomePageProps) {
+  console.log(posts);
   return (
     <Layout>
       <h1 className="">Hello World!</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
---
+++ types/Post.ts
@@ -0,0 +1,14 @@
+type Post = {
+  slug: string;
+  frontmatter: {
+    title: string;
+    date: string;
+    excerpt: string;
+    cover_image: string;
+    category: string;
+    author: string;
+    author_image: string;
+  };
+};
+
+export default Post;
