ff8601398c5f01f76b3c5fbad39277994dc75eea
Samuel Holt <samuel.l.holt@gmail.com>
2023-01-11 20:50:05 -0800

    Initial commit

---
+++ components/Layout.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+import Header from "@/components/Header";
+
+type LayoutProps = {
+  title: string;
+  keywords: string;
+  description: string;
+  children: ReactNode;
+};
+function Layout({ title, keywords, description, children }: LayoutProps) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main className="container mx-auto my-7">{children}</main>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Welcome to DevSpace",
+  keywords: "development, coding, programming",
+  description: "The best info and news in development",
+};
+
+export default Layout;
