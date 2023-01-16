40497ec6a4d2d464d62c9cfabdd8932c9d078f35
Samuel Holt <samuel.l.holt@gmail.com>
2023-01-15 16:18:13 -0800

    Added pagination routes for blog

---
+++ pages/blog/page/[page_index].tsx
@@ -0,0 +1,85 @@
+import Layout from "@/components/Layout";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import PostType from "@/types/PostType";
+import Post from "@/components/Post";
+import { sortByDate } from "@/utils/index";
+import { POSTS_PER_PAGE } from "@/config/index";
+
+type BlogPageProps = {
+  posts: PostType[];
+  currentPage: number;
+  numPages: number;
+};
+
+export default function BlogPage({
+  posts,
+  numPages,
+  currentPage,
+}: BlogPageProps) {
+  console.log(posts);
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+
+  let paths = [];
+  for (let i = 1; i <= numPages; i++) {
+    paths.push({ params: { page_index: i.toString() } });
+  }
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const page = parseInt((params && params.page_index) || 1);
+
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+  const pageIndex = page - 1;
+  const orderedPosts = posts
+    .sort(sortByDate)
+    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
+
+  return {
+    props: {
+      posts: orderedPosts,
+      numPages,
+      currentPage: page,
+    },
+  };
+}
